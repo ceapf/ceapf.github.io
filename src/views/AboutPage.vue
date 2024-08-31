6759cfd66de4ccaecb3f098dae83856678c0069a
ceapf <ceaemail.core@gmail.com>
2024-08-31 14:03:11 -0700

    Edited about page

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -3,17 +3,23 @@
       <h1>Bio</h1>
       <p>Bio Template Text</p>
       <h1>Education</h1>
+      <h3 style="margin-bottom: 50%;"><i>Degree</i></h3>
       <p><b><i>CSU Sacramento - BS Computer Science </i></b></p>
-      
-      <h2>Certificates</h2>
-      <p><b><i>CSU Sacramento - Scientific Computing & Simulation</i></b></p>
-      <p>Link Placeholder</p>
-      <p><b><i>Coursera - Neural Networks and Deep Learning</i></b></p>
-      <a href="https://coursera.org/verify/X8QLS289WEBX" target="_blank" style="font-size: 50%;">See Certificate</a>
-      <p><b><i>Coursera - Hyperparameter Tuning</i></b></p>
-      <a href="https://coursera.org/verify/HJ6REVMR4ARQ" target="_blank" style="font-size: 50%;">See Certificate</a>
-      <p><b><i>Cousera - Structuring Machine Learning Projects</i></b></p>
-      <a href="https://coursera.org/verify/9MPSG537USW5" target="_blank" style="font-size: 50%;">See Certificate</a>
+      <h3 style="margin-bottom: 50%;"><i>Relevant Coursework</i></h3>
+      <p style="font-size: 90%;">Course 1</p>
+      <p style="font-size: 90%;">Course 2</p>
+      <p style="font-size: 90%;">Course 3</p>
+      <p style="font-size: 90%;">Course 4</p>
+      <p style="font-size: 90%;">Course 5</p>
+      <h3 style="margin-bottom: 50%;">Certificates</h3>
+      <p style="margin-bottom: 50%;"><b><i>CSU Sacramento - Scientific Computing & Simulation</i></b></p>
+      <p style="font-size: 80%;">Link Placeholder</p>
+      <p style="margin-bottom: 50%;"><b><i>Coursera - Neural Networks and Deep Learning</i></b></p>
+      <a href="https://coursera.org/verify/X8QLS289WEBX" target="_blank" style="font-size: 80%;">See Certificate</a>
+      <p style="margin-bottom: 50%;"><b><i>Coursera - Hyperparameter Tuning</i></b></p>
+      <a href="https://coursera.org/verify/HJ6REVMR4ARQ" target="_blank" style="font-size: 80%;">See Certificate</a>
+      <p style="margin-bottom: 50%;"><b><i>Cousera - Structuring Machine Learning Projects</i></b></p>
+      <a href="https://coursera.org/verify/9MPSG537USW5" target="_blank" style="font-size: 80%;">See Certificate</a>
 
     </div>
   </template>
