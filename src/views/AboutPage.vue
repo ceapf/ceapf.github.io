bd96c9e35e34e4907c9d915c3468c1f03f828eb3
ceapf <ceaemail.core@gmail.com>
2024-08-31 14:21:45 -0700

    Edited about page

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -6,11 +6,14 @@
       <h3 style="margin-bottom: .5em;"><i>Degree</i></h3>
       <p><b><i>CSU Sacramento - BS Computer Science </i></b></p>
       <h3 style="margin-bottom: .5em;"><i>Relevant Coursework</i></h3>
-      <p style="font-size: 90%;">Course 1</p>
-      <p style="font-size: 90%;">Course 2</p>
-      <p style="font-size: 90%;">Course 3</p>
-      <p style="font-size: 90%;">Course 4</p>
-      <p style="font-size: 90%;">Course 5</p>
+      <p style="font-size: 90%;">Stat 50 - Intro to Probability & Statistics</p>
+      <p style="font-size: 90%;">CSc 131 - Computer Software Engineering</p>
+      <p style="font-size: 90%;">CSc 134 - Database Management Systems</p>
+      <p style="font-size: 90%;">CSc 163 - Scientific Computing: Modeling Simulation, and Visualization</p>
+      <p style="font-size: 90%;">CSc 180 - Intelligent Systems</p>
+      <p style="font-size: 90%;">CSc 174 - Advanced Database Management Systems</p>
+      <p style="font-size: 90%;">Stat 196k - Big Data</p>
+      <p style="font-size: 90%;">CSc 190 & 191 - Senior Project</p>
       <h3 style="margin-bottom: .5em;">Certificates</h3>
       <p style="margin-bottom: .5em;"><b><i>CSU Sacramento - Scientific Computing & Simulation</i></b></p>
       <p style="font-size: 80%;">Link Placeholder</p>
