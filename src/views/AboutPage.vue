c7a720073af7c17673a1a8abe3e3e638a25a4dbc
ceapf <ceaemail.core@gmail.com>
2024-08-31 13:45:20 -0700

    Edited about page

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,7 +1,21 @@
 <template>
     <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the home page.</p>
+      <h1>Bio</h1>
+      <p>Bio Template Text</p>
+      <h1>Education</h1>
+      <p><b><i>CSU Sacramento - BS Computer Science </i></b></p>
+      
+      <h2>Certificates</h2>
+      <br>
+      <p><b><i>Coursera - Neural Networks and Deep Learning</i></b></p>
+      <a href="https://coursera.org/verify/X8QLS289WEBX">See Certificate</a>"
+      <br>
+      <p><b><i>Coursera - Hyperparameter Tuning</i></b></p>
+      <a href="https://coursera.org/verify/HJ6REVMR4ARQ">See Certificate</a>
+      <br>
+      <p><b><i>Cousera - Structuring Machine Learning Projects</i></b></p>
+      <a href="https://coursera.org/verify/9MPSG537USW5">See Certificate</a>
+
     </div>
   </template>
   
