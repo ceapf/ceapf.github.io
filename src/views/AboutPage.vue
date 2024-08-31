4c0f5a4b36ba0d9460b0a9d49cbc9f392007bcb9
ceapf <ceaemail.core@gmail.com>
2024-08-31 13:51:51 -0700

    Edited about page

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -6,15 +6,14 @@
       <p><b><i>CSU Sacramento - BS Computer Science </i></b></p>
       
       <h2>Certificates</h2>
-      <br>
+      <p><b><i>CSU Sacramento - Scientific Computing & Simulation</i></b></p>
+      <p>Link Placeholder</p>
       <p><b><i>Coursera - Neural Networks and Deep Learning</i></b></p>
-      <a href="https://coursera.org/verify/X8QLS289WEBX">See Certificate</a>"
-      <br>
+      <a href="https://coursera.org/verify/X8QLS289WEBX" target="_blank">See Certificate</a>"
       <p><b><i>Coursera - Hyperparameter Tuning</i></b></p>
-      <a href="https://coursera.org/verify/HJ6REVMR4ARQ">See Certificate</a>
-      <br>
+      <a href="https://coursera.org/verify/HJ6REVMR4ARQ" target="_blank">See Certificate</a>
       <p><b><i>Cousera - Structuring Machine Learning Projects</i></b></p>
-      <a href="https://coursera.org/verify/9MPSG537USW5">See Certificate</a>
+      <a href="https://coursera.org/verify/9MPSG537USW5" target="_blank">See Certificate</a>
 
     </div>
   </template>
