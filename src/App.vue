6d29acfb008aaa262f558da4909c6956fdbc5679
ceapf <ceaemail.core@gmail.com>
2024-08-31 14:12:31 -0700

    added app padding

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  padding-top: 40px;
+  padding-top: 20px;
+  padding-left: 8px;
 }
 </style>
