ec0389cc942c82fe9d12bd9b1d64de729614cfc2
ceapf <ceaemail.core@gmail.com>
2024-08-31 14:28:01 -0700

    Edited about page

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -4,8 +4,8 @@
       <p>Bio Template Text</p>
       <h1>Education</h1>
       <h3 style="margin-bottom: .5em;"><i>Degree</i></h3>
-      <p><b><i>CSU Sacramento - BS Computer Science </i></b></p>
-      <h3 style="margin-bottom: .5em;"><i>Relevant Coursework</i></h3>
+      <p>CSU Sacramento - BS Computer Science</p>
+      <h3 style="margin-bottom: .5em;"><i>Relevant Coursework (to Data Science)</i></h3>
       <p style="font-size: 90%;">Stat 50 - Intro to Probability & Statistics</p>
       <p style="font-size: 90%;">CSc 131 - Computer Software Engineering</p>
       <p style="font-size: 90%;">CSc 134 - Database Management Systems</p>
@@ -14,14 +14,14 @@
       <p style="font-size: 90%;">CSc 174 - Advanced Database Management Systems</p>
       <p style="font-size: 90%;">Stat 196k - Big Data</p>
       <p style="font-size: 90%;">CSc 190 & 191 - Senior Project</p>
-      <h3 style="margin-bottom: .5em;">Certificates</h3>
-      <p style="margin-bottom: .5em;"><b><i>CSU Sacramento - Scientific Computing & Simulation</i></b></p>
+      <h3 style="margin-bottom: .5em;"><i>Certificates</i></h3>
+      <p style="margin-bottom: .5em;">CSU Sacramento - Scientific Computing & Simulation</p>
       <p style="font-size: 80%;">Link Placeholder</p>
-      <p style="margin-bottom: .5em;"><b><i>Coursera - Neural Networks and Deep Learning</i></b></p>
+      <p style="margin-bottom: .5em;">Coursera - Neural Networks and Deep Learning</p>
       <a href="https://coursera.org/verify/X8QLS289WEBX" target="_blank" style="font-size: 80%;">See Certificate</a>
-      <p style="margin-bottom: .5em;"><b><i>Coursera - Hyperparameter Tuning</i></b></p>
+      <p style="margin-bottom: .5em;">Coursera - Hyperparameter Tuning</p>
       <a href="https://coursera.org/verify/HJ6REVMR4ARQ" target="_blank" style="font-size: 80%;">See Certificate</a>
-      <p style="margin-bottom: .5em;"><b><i>Cousera - Structuring Machine Learning Projects</i></b></p>
+      <p style="margin-bottom: .5em;">Cousera - Structuring Machine Learning Projects</p>
       <a href="https://coursera.org/verify/9MPSG537USW5" target="_blank" style="font-size: 80%;">See Certificate</a>
 
     </div>
